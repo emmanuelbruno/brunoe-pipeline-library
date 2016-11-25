7e7b618f935e66865702730022cca28e56f3f5f3
Emmanuel Bruno <bruno@univ-tln.fr>
2016-11-25 17:09:51 +0100

    typo

--- vars/jenkinsFileLSISUtils.groovy
+++ vars/jenkinsFileLSISUtils.groovy
@@ -6,28 +6,28 @@ def branch = tokens[tokens.size() - 1]
 
 def slack_channel
 
-def setSlack_channel(slack_channel) {
-    this.slack_channel = slack_channel
+def setSlackChannel(slackChannel) {
+    this.slackChannel = slackChannel
 }
 
-def getSlack_channel() {
-    return this.slack_channel
+def getSlackChannel() {
+    return this.slackChannel
 }
 
-def setBuild_docker_image(build_docker_image) {
-    this.build_docker_image = build_docker_image
+def setMavenDockerImage(mavenDockerImage) {
+    this.mavenDockerImage = mavenDockerImage
 }
 
-def setPull_registry(pull_registry) {
-    this.pull_registry = pull_registry
+def setPullRegistry(pullRegistry) {
+    this.pullRegistry = pullRegistry
 }
 
-def setPrivate_registry(private_registry) {
-    this.private_registry = private_registry
+def setPrivateRegistry(privateRegistry) {
+    this.privateRegistry = privateRegistry
 }
 
-def setPublic_registry(public_registry) {
-    this.public_registry = public_registry
+def setPublicRegistry(publicRegistry) {
+    this.publicRegistry = publicRegistry
 }
 
 def setPom(pom) {
@@ -39,74 +39,57 @@ def setGitRemote(gitRemote) {
 }
 
 def init() {
+    slackSend channel: this.slackChannel,
+            color: "good",
+            message: "Build starting. <${env.BUILD_URL}|${env.JOB_NAME} ${env.BUILD_NUMBER}>)"
     checkout scm
     this.pom = readMavenPom file: 'pom.xml'
     this.gitRemote = sh(returnStdout: true, script: 'git remote get-url origin|cut -c9-').trim()
 }
 
-def build(UTLN_USERNAME, UTLN_PASSWORD) {
-    stage('Build') {
-        docker.image(this.build_docker_image)
-                .inside(
-                "-e http_proxy=http://${UTLN_USERNAME}:${UTLN_PASSWORD}@proxy.univ-tln.fr:3128 " +
-                        "-e https_proxy=http://${UTLN_USERNAME}:${UTLN_PASSWORD}@proxy.univ-tln.fr:3128 " +
-                        '-e DOCKER_HOST=tcp://172.18.0.1:2375 ' +
-                        '-v /var/run/docker.sock:/var/run/docker.sock ' +
-                        '-v /home/jenkins/.m2:/home/user/.m2 ' +
-                        '-v /home/jenkins/.docker:/home/user/.docker ') {
-            sh "mvn --settings /home/user/.m2/settings.xml " +
-                    "-Duser.home=/home/user " +
-                    "-B " +
-                    "-Ddocker.host=tcp://172.18.0.1:2375 " +
-                    "-Ddocker.username=${UTLN_USERNAME} " +
-                    "-Ddocker.password=${UTLN_PASSWORD} " +
-                    "-Ddocker.pull.registry=${pull_registry} " +
-                    "-Ddocker.buildArg.http_proxy=http://${UTLN_USERNAME}:${UTLN_PASSWORD}@proxy.univ-tln.fr:3128 " +
-                    "-Ddocker.buildArg.https_proxy=http://${UTLN_USERNAME}:${UTLN_PASSWORD}@proxy.univ-tln.fr:3128 " +
-                    "-Ddocker.buildArg.no_proxy=hub-docker.lsis.univ-tln.fr,.univ-tln.fr " +
-                    "package"
-        }
-        this.slackSend channel: slack_channel,
+def mvn(params) {
+    docker.image(this.mavenDockerImage)
+            .inside(
+            "-e http_proxy=http://${UTLN_USERNAME}:${UTLN_PASSWORD}@proxy.univ-tln.fr:3128 " +
+                    "-e https_proxy=http://${UTLN_USERNAME}:${UTLN_PASSWORD}@proxy.univ-tln.fr:3128 " +
+                    '-e DOCKER_HOST=tcp://172.18.0.1:2375 ' +
+                    '-v /var/run/docker.sock:/var/run/docker.sock ' +
+                    '-v /home/jenkins/.m2:/home/user/.m2 ' +
+                    '-v /home/jenkins/.docker:/home/user/.docker ') {
+        sh "mvn --settings /home/user/.m2/settings.xml " +
+                "-Duser.home=/home/user " +
+                "-B " +
+                "-Ddocker.host=tcp://172.18.0.1:2375 " +
+                "-Ddocker.username=${UTLN_USERNAME} " +
+                "-Ddocker.password=${UTLN_PASSWORD} " +
+                "-Ddocker.pull.registry=${pull_registry} " +
+                "-Ddocker.push.registry=${private_registry} " +
+                "-Ddocker.buildArg.http_proxy=http://${UTLN_USERNAME}:${UTLN_PASSWORD}@proxy.univ-tln.fr:3128 " +
+                "-Ddocker.buildArg.https_proxy=http://${UTLN_USERNAME}:${UTLN_PASSWORD}@proxy.univ-tln.fr:3128 " +
+                "-Ddocker.buildArg.no_proxy=hub-docker.lsis.univ-tln.fr,.univ-tln.fr " +
+                params
+    }
+}
+
+def mvnPackage() {
+    stage('Package') {
+        mvn("package")
+        slackSend channel: slackChannel,
                 color: "good",
                 message: "[<${env.BUILD_URL}|${this.pom.groupId}-${this.pom.artifactId}:${this.pom.version}>] builded."
     }
 }
 
-def deploy() {
+def mvnDeploy() {
     stage('Deploy') {
-        withCredentials([[$class          : 'UsernamePasswordMultiBinding',
-                          credentialsId   : 'bruno.utln',
-                          usernameVariable: 'UTLN_USERNAME',
-                          passwordVariable: 'UTLN_PASSWORD']]) {
-            docker.image(build_docker_image)
-                    .inside(
-                    "-e http_proxy=http://${env.UTLN_USERNAME}:${env.UTLN_PASSWORD}@proxy.univ-tln.fr:3128 " +
-                            "-e https_proxy=http://${env.UTLN_USERNAME}:${env.UTLN_PASSWORD}@proxy.univ-tln.fr:3128 " +
-                            '-e DOCKER_HOST=tcp://172.18.0.1:2375 ' +
-                            '-v /var/run/docker.sock:/var/run/docker.sock ' +
-                            '-v /home/jenkins/.m2:/home/user/.m2 ' +
-                            '-v /home/jenkins/.docker:/home/user/.docker ') {
-                sh "mvn --settings /home/user/.m2/settings.xml " +
-                        "-Duser.home=/home/user " +
-                        "-B " +
-                        "-Ddocker.host=tcp://172.18.0.1:2375 " +
-                        "-Ddocker.username=${env.UTLN_USERNAME} " +
-                        "-Ddocker.password=${env.UTLN_PASSWORD} " +
-                        "-Ddocker.pull.registry=${pull_registry} " +
-                        "-Ddocker.push.registry=${private_registry} " +
-                        "-Ddocker.buildArg.http_proxy=http://${env.UTLN_USERNAME}:${env.UTLN_PASSWORD}@proxy.univ-tln.fr:3128 " +
-                        "-Ddocker.buildArg.https_proxy=http://${env.UTLN_USERNAME}:${env.UTLN_PASSWORD}@proxy.univ-tln.fr:3128 " +
-                        "-Ddocker.buildArg.no_proxy=hub-docker.lsis.univ-tln.fr,.univ-tln.fr " +
-                        "deploy"
-            }
-        }
-        slackSend channel: slack_channel,
+        mvn("deploy")
+        slackSend channel: this.slackChannel,
                 color: "good",
                 message: "[<${env.BUILD_URL}|${pom.groupId}-${pom.artifactId}:${pom.version}>] pushed."
     }
 }
 
-def tag() {
+def gitTag() {
     stage('Tag to Github') {
         withCredentials([[$class          : 'UsernamePasswordMultiBinding',
                           credentialsId   : 'JenkinslsisGithub',
