d57ef44c2ad52e518e11ab6e98fd289a34037f31
Emmanuel Bruno <bruno@univ-tln.fr>
2016-12-01 11:05:19 +0100

    user a local cred for github

--- vars/jenkinsFileLSISUtils.groovy
+++ vars/jenkinsFileLSISUtils.groovy
@@ -118,7 +118,7 @@ def mvnQuality() {
 def gitTag() {
     stage('Git Tag') {
         withCredentials([[$class          : 'UsernamePasswordMultiBinding',
-                          credentialsId   : 'jenkinslsisgithub',
+                          credentialsId   : 'login.github',
                           usernameVariable: 'GIT_USERNAME',
                           passwordVariable: 'GIT_PASSWORD']]) {
             sh """
@@ -128,5 +128,39 @@ def gitTag() {
        git push https://${env.GIT_USERNAME}:${env.GIT_PASSWORD}@${gitRemote}  --tags
        """
         }
+    }
+}
+
+def defaultMavenFullPipeLine() {
+    node() {
+        try {
+            //In jenkins add settings.xml, settings-security.xml, login.utln (utln password)
+            //This file should be protected (signed ?)
+
+            jenkinsFileLSISUtils.slackChannel = "ci"
+            jenkinsFileLSISUtils.mavenDockerImage = 'hub-docker.lsis.univ-tln.fr:443/brunoe/maven:3-3.9-SNAPSHOT'
+
+            //checkout and set version with buildnumber
+            jenkinsFileLSISUtils.init()
+
+            //clean build package without tests
+            jenkinsFileLSISUtils.mvnBuild()
+            //run all tests
+            jenkinsFileLSISUtils.mvnTest()
+            jenkinsFileLSISUtils.mvnDeploy("-P stage-devel")
+            //check quality
+            jenkinsFileLSISUtils.mvnQuality()
+            jenkinsFileLSISUtils.mvnDeploy("-P stage-staging")
+            jenkinsFileLSISUtils.mvnDeploy("-P stage-production")
+
+            jenkinsFileLSISUtils.gitTag()
+        } catch (error) {
+            slackSend channel: jenkinsFileLSISUtils.slackChannel,
+                    color: "danger",
+                    message: "Build Error. <${env.BUILD_URL}|${env.JOB_NAME} ${env.BUILD_NUMBER}>) : ${error}"
+            throw error
+        } finally {
+        }
+
     }
 }
