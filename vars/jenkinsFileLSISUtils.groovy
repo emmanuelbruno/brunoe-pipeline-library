ea67a2495e8610d2050db18118e90fd2dd27e8b7
Emmanuel Bruno <emmanuel.bruno@univ-tln.fr>
2017-02-11 18:05:55 +0100

    typo

--- vars/jenkinsFileLSISUtils.groovy
+++ vars/jenkinsFileLSISUtils.groovy
@@ -136,7 +136,7 @@ def gitTag() {
             sh """
        git add .
        git commit -m 'Tag Jenkins Build'
-       git tag jenkins-${BRANCH}-${pom.artifactId}-${pom.version}
+       git tag jenkins-${pom.artifactId}-${BRANCH}-${pom.version}
        git push https://${env.GIT_USERNAME}:${env.GIT_PASSWORD}@${gitRemote}  --tags
        """
         }
